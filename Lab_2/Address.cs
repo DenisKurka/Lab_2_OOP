ef9602dc1fb5a10dbb17718b722a4beedb4430bf
Denis <deniskurkavol@gamil.com>
2022-09-27 19:06:36 +0300

    Lab2
    lab2

---
+++ Lab_2/Address.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab_2
+{
+    /*class Address
+    {
+        public int Index { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Apartment { get; set; }
+
+
+        public void DisplayName()
+        {
+            Console.WriteLine($"Index: {Index} \n Country : {Country} \n City : {City} \n Street : {Street} \n House : {House} \n Apartment : {Apartment}");
+        }
+
+        static void Main()
+        {
+            Address address = new Address();
+            address.Index = 18000;
+            address.Country = "Ukraine";
+            address.City = "Cherkasy";
+            address.Street = "Gagarina";
+            address.House = "23";
+            address.Apartment = "74";
+            address.DisplayName();
+        }
+    }*/
+}
